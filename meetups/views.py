30c96eaf4fa107bae69b0a97dbcb6e2526510d1d
hectorzaragoza <zaragoza09@gmail.com>
2021-12-22 09:42:49 -0800

    added css static pages

--- meetups/views.py
+++ meetups/views.py
@@ -5,5 +5,20 @@ from django.shortcuts import render
 # index will take in the request parameter automatically by django.
 def index(request):
     # return what you want to see when this views url is reached
+    meetups = [
+        { 
+            'title': 'A First Meetup',
+            'location': 'New York',
+            'slug': 'a-first-meetup'
+        },
+        { 
+            'title': 'A Second Meetup', 
+            'location': 'Paris',
+            'slug': 'a-second-meetup'
+        }
+    ]
     # render takes two arguments, request and the name of the template (path to template)
-    return render(request, 'meetups/index.html')
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
