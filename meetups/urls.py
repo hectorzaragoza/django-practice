d033535ba0c35b1fc2920411dc738330d9a30eea
hectorzaragoza <zaragoza09@gmail.com>
2021-12-22 07:52:04 -0800

    Added a template for meetups app to render an html file

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,7 +3,7 @@ from . import views
 # this is where we'll add the url patterns for this specific app
 # path will take two arguments, first is the url endpoint and second is to specify the view funciton to be called when that url is reached
 urlpatterns = [
-    path('meetups', views.index) # our-domain.com/meetups
+    path('meetups/', views.index) # our-domain.com/meetups
 ]
 
 # connect this app to the global folder 
--- meetups/views.py
+++ meetups/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 # index will take in the request parameter automatically by django.
-def index():
+def index(request):
     # return what you want to see when this views url is reached
-    return HttpResponse('Hello World')
+    # render takes two arguments, request and the name of the template (path to template)
+    return render(request, 'meetups/index.html')
