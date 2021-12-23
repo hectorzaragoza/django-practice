43753b5d466ca5a536252ad0db378273802ba59a
hectorzaragoza <zaragoza09@gmail.com>
2021-12-22 21:10:20 -0800

    Adding meetup details view

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,7 +3,8 @@ from . import views
 # this is where we'll add the url patterns for this specific app
 # path will take two arguments, first is the url endpoint and second is to specify the view funciton to be called when that url is reached
 urlpatterns = [
-    path('meetups/', views.index) # our-domain.com/meetups
+    path('meetups/', views.index), # our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details)
 ]
 
 # connect this app to the global folder 
