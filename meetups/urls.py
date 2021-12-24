c3a6a5239eb79e020e6f309426a93c06ef527026
hectorzaragoza <zaragoza09@gmail.com>
2021-12-24 14:15:20 -0800

    Configuring the Admin Page

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path    
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 # this is where we'll add the url patterns for this specific app
 # path will take two arguments, first is the url endpoint and second is to specify the view funciton to be called when that url is reached
 urlpatterns = [
     path('meetups/', views.index, name="all-meetups"), # our-domain.com/meetups
     path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-detail")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # connect this app to the global folder 
