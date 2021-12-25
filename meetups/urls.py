90af2abd3408d65eb3bac7f539d7b7c75262d951
hectorzaragoza <zaragoza09@gmail.com>
2021-12-25 07:58:28 -0800

    Completed Introduction to Django by creating a Meetups Website

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path    
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 # this is where we'll add the url patterns for this specific app
 # path will take two arguments, first is the url endpoint and second is to specify the view funciton to be called when that url is reached
 urlpatterns = [
-    path('meetups/', views.index, name="all-meetups"), # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-detail")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    path('', views.index, name="all-meetups"), # our-domain.com/meetups
+    path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+    path('<slug:meetup_slug>', views.meetup_details, name="meetup-detail"),
+] 
 # connect this app to the global folder 
